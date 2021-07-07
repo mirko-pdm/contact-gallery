227c368206000961639a964fb08b17d3a99bf31d
mirkox <mirkox90@gmail.com>
2021-07-07 20:37:36 +0200

    getting contact list

--- app/src/main/java/pdm/contactgallery/MainActivity.kt
+++ app/src/main/java/pdm/contactgallery/MainActivity.kt
@@ -1,11 +1,35 @@
 package pdm.contactgallery
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupPermissions()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.READ_CONTACTS)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                111)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
