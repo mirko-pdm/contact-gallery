0b74f90b79ce438c600d834ef62c30f4c92dd073
mirkox <mirkox90@gmail.com>
2021-07-08 03:29:11 +0200

    tab layout and fragments

--- app/src/main/java/pdm/contactgallery/MainActivity.kt
+++ app/src/main/java/pdm/contactgallery/MainActivity.kt
@@ -6,12 +6,49 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupPermissions()
         setContentView(R.layout.activity_main)
+
+
+        tabLayout = findViewById<TabLayout>(R.id.tabLayout).also {
+            it.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                    supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.also {
+                        supportFragmentManager.beginTransaction().remove(it).commit()
+                    }
+
+                    supportFragmentManager.commit {
+                        setReorderingAllowed(true)
+
+                        when(tab?.position) {
+                            0 -> replace<Fragment>(R.id.fragmentContainerView)
+                            1 -> replace<ContactListFragment>(R.id.fragmentContainerView)
+                        }
+                    }
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+
+            it.selectTab(it.getTabAt(savedInstanceState?.getInt("tabPosition") ?: 0))
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("tabPosition", tabLayout.selectedTabPosition)
     }
 
     private fun setupPermissions() {
